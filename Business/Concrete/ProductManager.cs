89eb1581dea1fdc57a320d839047a6f27aa194ea
ufukhacioglu <hacioglu.ufuk@gmail.com>
2022-09-04 01:02:13 +0300

    04.09.2022 Commit 1

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -32,7 +32,7 @@ namespace Business.Concrete
         }
 
         //Claim
-        [SecuredOperation("product.add")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
